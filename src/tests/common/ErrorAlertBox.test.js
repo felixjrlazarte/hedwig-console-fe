cb27e106c3cca36c1617a7684ae41fb60d212010
Felix Jr Lazarte <p.felix.lazarte@paymaya.com>
2023-02-07 13:42:05 +0800

    add initial unit test coverage for common components

---
+++ src/tests/common/ErrorAlertBox.test.js
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import ErrorAlertBox from '../../components/common/ErrorAlertBox';
+
+describe("ErrorAlertBox component", () => {
+  it("should render ErrorAlertBox component correctly", () => {
+    const { getByText } = render(<ErrorAlertBox title="ErrorAlertBox title"/>);
+
+    expect(getByText(/ErrorAlertBox title/i)).toBeInTheDocument();
+  });
+});
