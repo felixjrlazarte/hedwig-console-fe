3ca9a56f9b98f7f686ecc0e1040019a4bbd6aa5f
Felix Jr Lazarte <p.felix.lazarte@paymaya.com>
2023-02-15 14:38:37 +0800

    create blast details section

--- src/components/features/Blaster/SMS/SendSMS/SendSMSConfirmation.js
+++ src/components/features/Blaster/SMS/SendSMS/SendSMSConfirmation.js
@@ -3,7 +3,8 @@ import {
   Flex,
   Box,
   Text,
-  Text as Title,
+  Grid,
+  GridItem
 } from "@chakra-ui/react";
 import Button from "../../../../common/Button";
 import { ArrowForwardIcon, ArrowBackIcon } from "../../../../../assets/images/icons";
@@ -13,9 +14,27 @@ const SendSMSConfirmation = ({
   handleCancelAction,
   handlePreviousAction
 }) => {
+  const details = [
+    { label: "Name", value: blastDetails.blastName },
+    { label: "Audience", value: blastDetails.recipientType === "single" ? "Single Recipient" : "Multiple Recipients" },
+    { label: "Sender Mask", value: blastDetails.senderMask },
+    { label: "Message", value: blastDetails.blastMessage }
+  ];
+
+  const RenderBlastDetails = ({ label, value }) => (
+    <Grid templateColumns="repeat(5, 1fr)" mb="12px" key={label}>
+      <GridItem colSpan="2" color="text.darkgray" fontWeight="600" fontSize="14px">{label}</GridItem>
+      <GridItem colSpan="3" fontSize="14px">{value}</GridItem>
+    </Grid>
+  );
+
   return (
     <Box>
-      Your current settlement details are as follows:
+      <Text mb="16px">Your current settlement details are as follows:</Text>
+
+      <Box h="213px" borderRadius="4px" bg="#F6F8FE" p="20px">
+        { details.map((item) => RenderBlastDetails(item)) }
+      </Box>
 
       <Flex mt="64px" justifyContent="space-between">
         <Button width="auto" bg="none" color="button.primary" _hover={{ bg: "none", color: "bg.primary" }} onClick={handleCancelAction}>
