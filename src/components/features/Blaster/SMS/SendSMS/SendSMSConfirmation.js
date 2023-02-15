1430dceba8fc0f0a364c8f831f5468fae0f92ad4
Felix Jr Lazarte <p.felix.lazarte@paymaya.com>
2023-02-15 12:24:37 +0800

    add action buttons

--- src/components/features/Blaster/SMS/SendSMS/SendSMSConfirmation.js
+++ src/components/features/Blaster/SMS/SendSMS/SendSMSConfirmation.js
@@ -5,12 +5,32 @@ import {
   Text,
   Text as Title,
 } from "@chakra-ui/react";
+import Button from "../../../../common/Button";
+import { ArrowForwardIcon, ArrowBackIcon } from "../../../../../assets/images/icons";
 
-const SendSMSConfirmation = () => {
-
+const SendSMSConfirmation = ({
+  blastDetails,
+  handleCancelAction,
+  handlePreviousAction
+}) => {
   return (
     <Box>
       Your current settlement details are as follows:
+
+      <Flex mt="64px" justifyContent="space-between">
+        <Button width="auto" bg="none" color="button.primary" _hover={{ bg: "none", color: "bg.primary" }} onClick={handleCancelAction}>
+          Cancel
+        </Button>
+
+        <Flex justifyContent="space-between">
+          <Button width="156px" mr="12px" variant="outlined" icon={ArrowBackIcon} onClick={handlePreviousAction}>
+            Previous
+          </Button>
+          <Button type="submit" width="201px" rightIcon={ArrowForwardIcon}>
+            Send SMS Blast
+          </Button>
+        </Flex>
+      </Flex>
     </Box>
   );
 };
