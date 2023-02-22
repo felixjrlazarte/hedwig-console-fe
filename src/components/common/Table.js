46773838a5defa54552ec96bce91c9846cf03dc4
Felix Jr Lazarte <p.felix.lazarte@paymaya.com>
2023-02-22 14:57:35 +0800

    dynamic table creation

--- src/components/common/Table.js
+++ src/components/common/Table.js
@@ -9,33 +9,44 @@ import {
   Th
 } from "@chakra-ui/react";
 
+const headers = [
+  { key: "date", displayText: "Date" },
+  { key: "name", displayText: "Name" },
+  { key: "id", displayText: "ID" },
+  { key: "senderMask", displayText: "Sender Mask" },
+  { key: "status", displayText: "Status" }
+];
+
+const data = [
+  { date: "2021-07-08 00:12:09", name: "OTC SMS Blast Batch 1", id: "b93af4bdd76c", senderMask: "MayaRewards", status: "Completed" },
+  { date: "2021-07-08 00:12:09", name: "OTC SMS Blast Batch 1", id: "b93af4bdd76c", senderMask: "MayaRewards", status: "Completed" }
+];
+
 const Table = () => {
   return (
     <TableContainer>
       <ChakraTable variant='simple'>
         <Thead>
           <Tr>
-            <Th>To convert</Th>
-            <Th>into</Th>
-            <Th>multiply by</Th>
+            {
+              headers.map(({ key, displayText }) => (
+                <Th key={key} borderColor="#E0E4E6">{displayText}</Th>
+              ))
+            }
           </Tr>
         </Thead>
-        <Tbody bg="#F7F7F7">
-          <Tr>
-            <Td>inches</Td>
-            <Td>millimetres (mm)</Td>
-            <Td>25.4</Td>
-          </Tr>
-          <Tr>
-            <Td>feet</Td>
-            <Td>centimetres (cm)</Td>
-            <Td>30.48</Td>
-          </Tr>
-          <Tr>
-            <Td>yards</Td>
-            <Td>metres (m)</Td>
-            <Td>0.91444</Td>
-          </Tr>
+        <Tbody bg="#F7F7F7" fontSize="14px" fontWeight={400}>
+          {
+            data.map((row, index) => (
+              <Tr key={`table-row-${index}`}>
+                {
+                  headers.map(({ key }) => (
+                    <Td key={`table-data-${index}-${key}`} borderColor="#E0E4E6">{row[key]}</Td>
+                  ))
+                }
+              </Tr>
+            ))
+          }
         </Tbody>
       </ChakraTable>
     </TableContainer>
