3d59628c13232b3c36d665dde2627f34e87c328e
Felix Jr Lazarte <p.felix.lazarte@paymaya.com>
2023-02-23 15:54:35 +0800

    initial commit for success prompt

--- src/components/features/Blaster/SMS/index.js
+++ src/components/features/Blaster/SMS/index.js
@@ -11,35 +11,43 @@ import {
   Box as Header
 } from "@chakra-ui/react";
 import Button from "../../../common/Button";
+import AlertBox from "../../../common/AlertBox";
 import { AddIcon, BlasterIconPurple, ChevronRightIcon } from "../../../../assets/images/icons";
 
 const SMS = () => {
   const navigate = useNavigate();
 
   return (
-    <Box bg="white" w="full" borderRadius="xl" borderWidth="1px" borderColor="#E0E4E6">
-      <Header px="24px" py="28px">
-        <Breadcrumb fontSize={"14px"} spacing="8px" mb="28px" separator={<img src={ChevronRightIcon} alt="right-icon" width={20} height={20} />}>
-          <BreadcrumbItem>
-            <img src={BlasterIconPurple} alt="blaster-icon" width={20} height={20} />
-            <BreadcrumbLink color="text.link" ml="8px" href="#">Blaster</BreadcrumbLink>
-          </BreadcrumbItem>
+    <Box>
+      <AlertBox
+        type="warning"
+        message="SMS Sent! Please monitor the status to check if the SMS Blast has completed."
+      />
 
-          <BreadcrumbItem isCurrentPage>
-            <BreadcrumbLink href="#">SMS</BreadcrumbLink>
-          </BreadcrumbItem>
-        </Breadcrumb>
+      <Box bg="white" w="full" borderRadius="xl" borderWidth="1px" borderColor="#E0E4E6">
+        <Header px="24px" py="28px">
+          <Breadcrumb fontSize={"14px"} spacing="8px" mb="28px" separator={<img src={ChevronRightIcon} alt="right-icon" width={20} height={20} />}>
+            <BreadcrumbItem>
+              <img src={BlasterIconPurple} alt="blaster-icon" width={20} height={20} />
+              <BreadcrumbLink color="text.link" ml="8px" href="#">Blaster</BreadcrumbLink>
+            </BreadcrumbItem>
 
-        <Flex justifyContent="space-between">
-          <Title ml="8px" fontSize="32px">SMS</Title>
-          <Button icon={AddIcon} onClick={() => navigate("send")}>Send an SMS Blast</Button>
-        </Flex>
-      </Header>
+            <BreadcrumbItem isCurrentPage>
+              <BreadcrumbLink href="#">SMS</BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
 
-      <Divider mb="16px" opacity={1} />
+          <Flex justifyContent="space-between">
+            <Title ml="8px" fontSize="32px">SMS</Title>
+            <Button icon={AddIcon} onClick={() => navigate("send")}>Send an SMS Blast</Button>
+          </Flex>
+        </Header>
 
-      <Box h="680px">
+        <Divider mb="16px" opacity={1} />
 
+        <Box h="680px">
+  
+        </Box>
       </Box>
     </Box>
   );
