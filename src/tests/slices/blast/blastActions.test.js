6264ed1ccbb4d1f2b1b9a1bc31da72fdac9d45d8
Felix Jr Lazarte <p.felix.lazarte@paymaya.com>
2023-03-30 10:03:01 +0800

    add unit testing coverage

--- src/tests/slices/blast/blastActions.test.js
+++ src/tests/slices/blast/blastActions.test.js
@@ -4,7 +4,8 @@ import {
   getSenderMasks,
   sendSMSBlast,
   getBlastActivityList,
-  getBlastDetails
+  getBlastDetails,
+  downloadBlastFile
 } from "../../../slices/blast/blastActions";
 
 const store = configureStore({
@@ -14,6 +15,7 @@ const store = configureStore({
       case "blasts/fulfilled":
       case "blastActivityList/fulfilled":
       case "blastDetails/fulfilled":
+      case "downloadBlastFile/fulfilled":
         return action.payload;
       default:
         return state;
@@ -68,7 +70,7 @@ describe("BlastActions", () => {
     });
   });
 
-  describe("/blasts/details/{id}", () => {
+  describe("/blasts/list/", () => {
     it("should test success callback", async () => {
       const params = { page: 1, limit: 10 };
       const getSuccess = jest.spyOn(axios, "get").mockResolvedValueOnce({ data: { id: "123" } });
@@ -106,6 +108,36 @@ describe("BlastActions", () => {
 
       jest.spyOn(axios, "get").mockRejectedValueOnce({ response: {} });
       const response2 = await store.dispatch(getBlastDetails());
+
+      expect(response2.error.message).toBe("Rejected");
+    });
+  });
+
+  describe("/blasts/${blastId}/download", () => {
+    it("should test success callback", async () => {
+      const getSuccess = jest.spyOn(axios, "get").mockResolvedValueOnce({ data: { message: "123" } });
+      await store.dispatch(downloadBlastFile({
+        blastId: "12345",
+        params: { type: "invalid" }
+      }));
+
+      expect(getSuccess).toBeCalledWith("/blasts/12345/download", { params: { type: "invalid" } });
+    });
+
+    it("should test failed callback", async () => {
+      jest.spyOn(axios, "get").mockRejectedValueOnce({ response: { data: "error" } });
+      const response = await store.dispatch(downloadBlastFile({
+        blastId: "12345",
+        params: { type: "invalid" }
+      }));
+
+      expect(response.error.message).toBe("Rejected");
+
+      jest.spyOn(axios, "get").mockRejectedValueOnce({ response: {} });
+      const response2 = await store.dispatch(downloadBlastFile({
+        blastId: "12345",
+        params: { type: "invalid" }
+      }));
 
       expect(response2.error.message).toBe("Rejected");
     });
