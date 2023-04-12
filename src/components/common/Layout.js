7ff59a5cc181313dd1fbd91a4f979217a5b5565c
Felix Jr Lazarte <p.felix.lazarte@paymaya.com>
2023-04-12 15:43:26 +0800

    HC-297 add idle handler for auto logout

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useIdleTimer } from "react-idle-timer";
 import { Outlet, Navigate } from "react-router-dom";
 import { Box, Center, Text, Flex } from "@chakra-ui/react";
 
 import { configState } from "../../slices/config";
 import { userState } from "../../slices/user/userSlice";
 import { blastState } from "../../slices/blast/blastSlice";
-import { getUserInfo } from "../../slices/user/userActions";
+import { getUserInfo, resetUserState } from "../../slices/user/userActions";
 import { getSenderMasks } from "../../slices/blast/blastActions";
 
 import Header from "./Header";
@@ -15,6 +16,36 @@ import Loader from "./Loader";
 import Logo from "../../assets/images/hc_logo_purple.svg";
 
 import { isEmpty } from "../../utils/helpers";
+
+const TIMEOUT_IN_MINUTES = 30;
+
+/* istanbul ignore next */
+const IdleHandler = ({ children }) => {
+  const dispatch = useDispatch();
+
+  const onPromptState = (resetTimer) => {
+    dispatch(getUserInfo()).unwrap()
+      .then(() => resetTimer())
+      .catch(() => dispatch(resetUserState()));
+  };
+
+  const { activate } = useIdleTimer({
+    onPrompt: () => onPromptState(activate),
+    timeout: 1000 * 60 * TIMEOUT_IN_MINUTES,
+    promptBeforeIdle: 1000,
+    throttle: 500
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => { }, 500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
+  return children;
+};
 
 const Layout = ({ authorize, type }) => {
   const dispatch = useDispatch();
@@ -53,27 +84,31 @@ const Layout = ({ authorize, type }) => {
 
   if (type === "modal") {
     return (
-      <Flex minH="100vh" bg={"bg.gray.100"} flexDirection="column" alignItems="center">
-        <Center h="56px" bg="white" w="full" position="fixed" zIndex={1}>
-          <img src={Logo} alt="blaster-icon" width={28} height={32} />
-          <Text ml="8px" fontSize="24px" color="bg.secondary" fontFamily="sans-serif" fontWeight="bold">HEDWIG</Text>
-        </Center>
-        <Box w="620px" bg="white" py="40px" px="24px" mt="81px" mb="25px" borderRadius="lg" borderWidth="1px" borderColor="#E0E4E6">
-          <Outlet />
-        </Box>
-      </Flex>
+      <IdleHandler>
+        <Flex minH="100vh" bg={"bg.gray.100"} flexDirection="column" alignItems="center">
+          <Center h="56px" bg="white" w="full" position="fixed" zIndex={1}>
+            <img src={Logo} alt="blaster-icon" width={28} height={32} />
+            <Text ml="8px" fontSize="24px" color="bg.secondary" fontFamily="sans-serif" fontWeight="bold">HEDWIG</Text>
+          </Center>
+          <Box w="620px" bg="white" py="40px" px="24px" mt="81px" mb="25px" borderRadius="lg" borderWidth="1px" borderColor="#E0E4E6">
+            <Outlet />
+          </Box>
+        </Flex>
+      </IdleHandler>
     );
   }
 
   return (
-    <Box bg={"bg.gray.100"}>
-      <Loader isLoading={isUserLoading || isBlastLoading} />
-      <Sidebar display="block" isToggle={isToggle} sidebarWidth={sidebarWidth} toggleSidebar={handleToggle} />
-      <Header />
-      <Box transition="0.5s ease" ml={sidebarWidth} p="92px 36px 36px 36px" h="100vh" overflow="scroll">
-        <Outlet />
+    <IdleHandler>
+      <Box bg={"bg.gray.100"}>
+        <Loader isLoading={isUserLoading || isBlastLoading} />
+        <Sidebar display="block" isToggle={isToggle} sidebarWidth={sidebarWidth} toggleSidebar={handleToggle} />
+        <Header />
+        <Box transition="0.5s ease" ml={sidebarWidth} p="92px 36px 36px 36px" h="100vh" overflow="scroll">
+          <Outlet />
+        </Box>
       </Box>
-    </Box>
+    </IdleHandler>
   );
 };
 
